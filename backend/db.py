5431a83170c6bb1858e8048e0c230c63fd1e29aa
Adebayo Philip <114924407+coderrrrr2@users.noreply.github.com>
2025-04-11 20:54:56 +0100

    changed db setup

--- backend/db.py
+++ backend/db.py
@@ -1,10 +1,17 @@
+import os
 import psycopg2
 
+# def get_connection():
+#     return psycopg2.connect(
+#         host="localhost",
+#         port=5432,
+#         database="microscope_db",
+#         user="postgres",
+#         password="password"  # <-- Replace this with your actual password
+#     )
+
+
 def get_connection():
-    return psycopg2.connect(
-        host="localhost",
-        port=5432,
-        database="microscope_db",
-        user="postgres",
-        password="password"  # <-- Replace this with your actual password
-    )
+    return psycopg2.connect(os.environ["DATABASE_URL"])
+
+
